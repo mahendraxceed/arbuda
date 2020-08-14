aff7f2fc9a7c3db60c0b9c341e6f3ac9c3acc40c
mahendra <mahendra@xceednet.com>
2020-08-14 10:41:13 +0530

    default user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email:'mahendra@gmail.com', password: "mahi@123$", name: "mahendra dabhi", mobile: "7043245206", admin: true)
